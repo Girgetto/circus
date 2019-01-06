f8eac2178233c40e67506bc4ccc9e608d0041779
Giorgio Grassini <giorgiograssini@gmail.com>
2019-01-06 20:30:33 +0100

    Updates

---
+++ precache-manifest.bc7e07867c2e7a4f7272dbfde8eba1a0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f70c902826f228e4947769c1f4ba3fb",
+    "url": "/static/media/fourth.0f70c902.jpg"
+  },
+  {
+    "revision": "c085b442464b1b094bd9",
+    "url": "/static/css/main.45a0f08b.chunk.css"
+  },
+  {
+    "revision": "b9cb721b8454189c549e",
+    "url": "/static/js/1.b9cb721b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "428faf91619b971e53b6d4537e8eccb8",
+    "url": "/static/media/circus.428faf91.svg"
+  },
+  {
+    "revision": "e4a67dc29848acdee8776ae05413c426",
+    "url": "/static/media/spain.e4a67dc2.svg"
+  },
+  {
+    "revision": "ded1433a3192001cfffe9d978bc5f829",
+    "url": "/static/media/portugal.ded1433a.svg"
+  },
+  {
+    "revision": "fa76e43c2c91c37bc291053e818a78f6",
+    "url": "/static/media/menu.fa76e43c.svg"
+  },
+  {
+    "revision": "373b085577dc856037ad3ce6d8aa6829",
+    "url": "/static/media/first.373b0855.jpg"
+  },
+  {
+    "revision": "43259805a75a4c676da06671f9f133fa",
+    "url": "/static/media/second.43259805.jpg"
+  },
+  {
+    "revision": "0d975f3d89fe12b73522451f59374304",
+    "url": "/static/media/red.0d975f3d.svg"
+  },
+  {
+    "revision": "c085b442464b1b094bd9",
+    "url": "/static/js/main.c085b442.chunk.js"
+  },
+  {
+    "revision": "ebe7082948f3d820ecd52d915e3979e3",
+    "url": "/static/media/Fifth.ebe70829.svg"
+  },
+  {
+    "revision": "b7b86c8e1f825eb236f202858b5ae90b",
+    "url": "/static/media/emilio-garcia.b7b86c8e.png"
+  },
+  {
+    "revision": "1f85c8c27ea1361852173fb791b1213a",
+    "url": "/static/media/first.1f85c8c2.svg"
+  },
+  {
+    "revision": "0ca739f7e4f13f038af8e31a0209642b",
+    "url": "/static/media/ironHack.0ca739f7.svg"
+  },
+  {
+    "revision": "3cea30e49b2bc700ac8557b14663712e",
+    "url": "/static/media/campusMadrid.3cea30e4.svg"
+  },
+  {
+    "revision": "39ec3b734cda709ffe219d430874db24",
+    "url": "/static/media/cabify.39ec3b73.svg"
+  },
+  {
+    "revision": "233babf1eabfc9cd23750843dbe51d29",
+    "url": "/static/media/opinator.233babf1.svg"
+  },
+  {
+    "revision": "41b164e2a1405b8774119675ff8c6a10",
+    "url": "/static/media/circus.41b164e2.svg"
+  },
+  {
+    "revision": "56213e6109b512f482cd2816887d9105",
+    "url": "/static/media/ironHackFooter.56213e61.svg"
+  },
+  {
+    "revision": "2e1076dcc4b8f5b15ad715ae16514131",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/circus/precache-manifest.7a64824c90fbea0b547ea65e0db2a855.js"
+  "/precache-manifest.bc7e07867c2e7a4f7272dbfde8eba1a0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/circus/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
