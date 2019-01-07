d824d710762fe07432114f92f4255c9dfcea6acf
Giorgio Grassini <giorgiograssini@gmail.com>
2019-01-07 13:18:00 +0100

    hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
 
