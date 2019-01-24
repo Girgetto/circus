553cecb56a80cf583f4faffceaaafb3b68f9b117
Giorgio Grassini <giorgiograssini@gmail.com>
2019-01-24 11:52:50 +0100

    modified h2 in media query

--- src/components/ProximasFechas/style.js
+++ src/components/ProximasFechas/style.js
@@ -94,6 +94,8 @@ export const InnerLiDiv = styled.div`
 `;
 
 export const InnerTitleDiv = styled.div`
+  display: flex;
+  justify-content: center;
   flex-direction: column;
   width: 100%;
   background-color: ${props => (props.index === `${props.i}` ? props.color : '')};
