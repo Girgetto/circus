7be0e7f7f5b5fb2e367e897184f681e5fb6308b7
Giorgio Grassini <giorgiograssini@gmail.com>
2019-02-01 18:35:25 +0100

    Updates

---
+++ precache-manifest.f2b85e3f15f8d98e53abce8f8d04b371.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebe7082948f3d820ecd52d915e3979e3",
+    "url": "/static/media/Fifth.ebe70829.svg"
+  },
+  {
+    "revision": "ce2fbc3bd2e75959136b",
+    "url": "/static/css/main.df2ceaa8.chunk.css"
+  },
+  {
+    "revision": "15b3740a57bf1f720075",
+    "url": "/static/js/1.15b3740a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "428faf91619b971e53b6d4537e8eccb8",
+    "url": "/static/media/circus.428faf91.svg"
+  },
+  {
+    "revision": "e4a67dc29848acdee8776ae05413c426",
+    "url": "/static/media/spain.e4a67dc2.svg"
+  },
+  {
+    "revision": "ded1433a3192001cfffe9d978bc5f829",
+    "url": "/static/media/portugal.ded1433a.svg"
+  },
+  {
+    "revision": "fa76e43c2c91c37bc291053e818a78f6",
+    "url": "/static/media/menu.fa76e43c.svg"
+  },
+  {
+    "revision": "19f55a5afa46fff4ea4b57e60a5f083e",
+    "url": "/static/media/first.19f55a5a.jpg"
+  },
+  {
+    "revision": "4d4cde3c5cc396a8bb06798eb58a7cd4",
+    "url": "/static/media/second.4d4cde3c.jpg"
+  },
+  {
+    "revision": "0d975f3d89fe12b73522451f59374304",
+    "url": "/static/media/red.0d975f3d.svg"
+  },
+  {
+    "revision": "6bae38ad3f54e26e7a9ffd23c9ac024b",
+    "url": "/static/media/Francisco.6bae38ad.png"
+  },
+  {
+    "revision": "ee337146fb6c4238daab92ccdf46a497",
+    "url": "/static/media/Xue.ee337146.png"
+  },
+  {
+    "revision": "c953d33025c06ca16c4173e7bb5e4bc9",
+    "url": "/static/media/Javier.c953d330.png"
+  },
+  {
+    "revision": "ce2fbc3bd2e75959136b",
+    "url": "/static/js/main.ce2fbc3b.chunk.js"
+  },
+  {
+    "revision": "b3857492688f6f32822a56058578b166",
+    "url": "/static/media/first.b3857492.svg"
+  },
+  {
+    "revision": "3a9d522b21306d6482fcfa84898a04fd",
+    "url": "/static/media/second.3a9d522b.svg"
+  },
+  {
+    "revision": "f1078f88956a212912de75878de17b3f",
+    "url": "/static/media/third.f1078f88.svg"
+  },
+  {
+    "revision": "593668df9202864b6277b80652846a9b",
+    "url": "/static/media/fourth.593668df.svg"
+  },
+  {
+    "revision": "0ca739f7e4f13f038af8e31a0209642b",
+    "url": "/static/media/ironHack.0ca739f7.svg"
+  },
+  {
+    "revision": "3cea30e49b2bc700ac8557b14663712e",
+    "url": "/static/media/campusMadrid.3cea30e4.svg"
+  },
+  {
+    "revision": "39ec3b734cda709ffe219d430874db24",
+    "url": "/static/media/cabify.39ec3b73.svg"
+  },
+  {
+    "revision": "233babf1eabfc9cd23750843dbe51d29",
+    "url": "/static/media/opinator.233babf1.svg"
+  },
+  {
+    "revision": "884ce20492c3f3d2cf76566394c1ca31",
+    "url": "/static/media/CARTO-logo-positive.884ce204.svg"
+  },
+  {
+    "revision": "27e54cff4a50abcbdc8390334d413d56",
+    "url": "/static/media/playtomic_imagotipo_RGB.27e54cff.png"
+  },
+  {
+    "revision": "41b164e2a1405b8774119675ff8c6a10",
+    "url": "/static/media/circus.41b164e2.svg"
+  },
+  {
+    "revision": "56213e6109b512f482cd2816887d9105",
+    "url": "/static/media/ironHackFooter.56213e61.svg"
+  },
+  {
+    "revision": "eea3019002c748d5646b05f5471dfa91",
+    "url": "/index.html"
+  }
+];
