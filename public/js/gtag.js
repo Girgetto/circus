905520abd2898a33af708db386ca564fd73edc47
Giorgio Grassini <giorgiograssini@gmail.com>
2019-02-11 11:18:14 +0100

    google analytics

--- public/js/gtag.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125567978-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-125567978-2');
-</script>
